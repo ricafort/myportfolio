3d1040f02d2e000ede929a50f97eb451bbbe4c10
ricafort <ricafort@gmail.com>
2023-11-12 14:03:21 +1100

    ok

--- force-app/main/default/lwc/bmiCalculator/bmiCalculator.js
+++ force-app/main/default/lwc/bmiCalculator/bmiCalculator.js
@@ -3,7 +3,8 @@ import { LightningElement } from 'lwc';
 export default class BmiCalculator extends LightningElement {
     height = ''
     weight = ''
-
+    bmiValue = ''
+    result = ''
     inputHandler(event){
         
         const{name, value} = event.target
@@ -31,6 +32,23 @@ export default class BmiCalculator extends LightningElement {
         //BMI = weight in KG / (height in m * height in cm)
         let height = Number(this.height)/100;
         let bmi = Number(this.weight) / (height * height);
+       
+        this.bmiValue = Number(bmi.toFixed(2))
+
+        if(this.bmiValue < 18.5){
+            this.result = "Underweight"
+        }
+        else if(this.bmiValue >= 18.5 && this.bmiValue < 25){
+            this.result = "Healthy"
+        }
+        else if(this.bmiValue >= 25 && this.bmiValue < 30){
+            this.result = "Overweight"
+        }
+        else{
+            this.result = "Obese"
+        }
+        
         console.log("BMI value is", bmi)
+        console.log("Result is", this.result)
     }
 }
